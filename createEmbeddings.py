f38012bb0b12dbc37039e5b4aedffdea596cd2d4
YashK2005 <yashkoth7@gmail.com>
2024-02-19 01:49:24 -0500

    Used pandas to cleanup data, created embeddings for data using OpenAI API, stored embeddings in Pinecone.

---
+++ createEmbeddings.py
@@ -0,0 +1,69 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import tiktoken
+import matplotlib.pyplot as plt
+from pinecone import Pinecone
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_KEY")
+MODEL = "text-embedding-3-small"
+
+client = OpenAI(
+    api_key=OPENAI_API_KEY
+)
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+INDEX = pc.Index("mail")
+
+
+def token_counter(df): #counting tokens for the email text so I don't spend too much money
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    df['n_tokens'] = df.body.apply(lambda x: len(tokenizer.encode(x)))
+    df.n_tokens.hist()
+    print(df.n_tokens.describe())
+    plt.show()
+
+def remove_newlines(serie): #cleaning up some data
+    serie = serie.str.replace('\n', ' ')
+    serie = serie.str.replace('\\n', ' ')
+    serie = serie.str.replace('  ', ' ')
+    serie = serie.str.replace('  ', ' ')
+    return serie
+
+def read_csv(): #reading the csv file
+    df = pd.read_csv('email.csv')
+    df['body'] = remove_newlines(df.body)
+    df = df.fillna('nan') #filling in missing values
+    return df
+
+def create_embeddings(df, model, index): #creating embeddings for the email text and uploading to pinecone
+    BATCH_SIZE = 50
+    for i in range(0, len(df), BATCH_SIZE):
+        print(f"Processing {i} to {i+BATCH_SIZE}...")
+        res = client.embeddings.create(
+            input=df.body[i:i+BATCH_SIZE], model=model
+        )
+        embeds = [record.embedding for record in res.data]
+        ids = df.id[i:i+BATCH_SIZE]
+        meta = df[['subject', 'from', 'to']][i:i+BATCH_SIZE].to_dict(orient='records')
+        to_upsert = zip(ids, embeds, meta)
+        index.upsert(vectors=list(to_upsert))
+        print("tokens used: ", res.usage.total_tokens)
+    print("DONE!")
+
+
+df = read_csv()
+#token_counter(df.head(1000))
+
+#create_embeddings(df[1400:2000], MODEL, INDEX)
+
+res = client.embeddings.create(
+    input="How can I learn about how artificial intelligence is impacting the healthcare field?", model=MODEL    
+)
+print("\n\n")
+print(res.data[0].embedding)
+
+print(df.head()[1:50].subject)
--- fetchMail.py
+++ fetchMail.py
@@ -19,13 +19,11 @@ from bs4 import BeautifulSoup
 from bs4 import BeautifulSoup
 import re
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 creds = None
-# The file token.json stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
+# The file token.json stores the user's access and refresh tokens
+
 if os.path.exists('token.json'):
     try:
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -34,6 +32,7 @@ if os.path.exists('token.json'):
         # if refresh token fails, reset creds to none.
         creds = None
         print(f'An error occurred: {error}')
+
 # If there are no (valid) credentials available, let the user log in.
 if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
@@ -42,14 +41,13 @@ if not creds or not creds.valid:
         flow = InstalledAppFlow.from_client_secrets_file(
             'credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
-       # creds = flow.run_console()
     # Save the credentials for the next run
     with open('token.json', 'w') as token:
         token.write(creds.to_json())
 
 
 emails = [] # indvidiual raw emails stored
-CUT_OFF = 50 # maximum number of emails to be downloaded
+CUT_OFF = 10000 # maximum number of emails to be downloaded
 try:
   service = build("gmail", "v1", credentials=creds)
   gmail_messages = service.users().messages()
@@ -87,39 +85,42 @@ content_dictionary = {
 
 counter = 0
 for email in emails:
-    msg_str = base64.urlsafe_b64decode(email['raw'].encode('ASCII'))
-  
-    subject = from_email = to_email = None
-    msg = parser.parsestr(msg_str.decode('utf-8'))
-    for key in msg.keys():
-        if key == "Subject":
-            subject = msg.get_all("Subject")
-        if key == "From":
-            from_email = msg.get_all("From")
-        if key == "To":
-            to_email = msg.get_all("To")
-    print(f"from: {from_email}, subject: {subject}")
-    for part in msg.walk():
-        if part.get_content_type() == "text/html":
-            content = part.get_content()
-            soup = BeautifulSoup(content, 'html.parser')
-            clear_content = soup.get_text(separator=' ', strip=True)
-            cleaned_mssg_body = clear_content.replace('\n', '').replace('\r', '')
-            content_dictionary[counter] = {
-                "id": email["id"],
-                "subject": subject,
-                "from": from_email,
-                "to": to_email,
-                "body": cleaned_mssg_body,
-                #"snippet": email["snippet"]
-            }
-            counter += 1
-
-print(content_dictionary)
+    try:
+        msg_str = base64.urlsafe_b64decode(email['raw'].encode('ASCII'))
+    
+        subject = from_email = to_email = None
+        msg = parser.parsestr(msg_str.decode('utf-8'))
+        for key in msg.keys():
+            if key == "Subject":
+                subject = msg.get_all("Subject")
+            if key == "From":
+                from_email = msg.get_all("From")
+            if key == "To":
+                to_email = msg.get_all("To")
+        #print(f"from: {from_email}, subject: {subject}")
+        for part in msg.walk():
+            if part.get_content_type() == "text/html":
+                content = part.get_content()
+                soup = BeautifulSoup(content, 'html.parser')
+                clear_content = soup.get_text(separator=' ', strip=True)
+                cleaned_mssg_body = clear_content.replace('\n', '').replace('\r', '')
+                content_dictionary[counter] = {
+                    "id": email["id"],
+                    "subject": subject,
+                    "from": from_email,
+                    "to": to_email,
+                    "body": cleaned_mssg_body,
+                    #"snippet": email["snippet"]
+                }
+                counter += 1
+    except:
+        counter += 1
+        print("Eror in parsing email")
+#print(content_dictionary)
 
 
 #exporting the values as .csv
-with open('emails.csv', 'w', encoding='utf-8', newline = '') as csvfile: 
+with open('email.csv', 'w', encoding='utf-8', newline = '') as csvfile: 
     fieldnames = ['id','subject','from','to','body']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter = ',')
     writer.writeheader()
